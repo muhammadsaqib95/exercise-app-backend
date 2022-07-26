d71d3044691cac87b7817be43c4d56953fbacbf4
saqib <saqib@apptimumlabs.net>
2022-07-26 19:20:01 +0500

    changes in server file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const multer = require('multer');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const multer = require("multer");
 var upload = multer();
 
 const app = express();
@@ -11,20 +11,20 @@ const port = process.env.PORT || 9000;
 app.use(cors());
 app.use(express.json());
 // for parsing multipart/form-data
-app.use(upload.array()); 
+app.use(upload.array());
 
 const uri = process.env.ATLAS_URI;
- mongoose.connect(uri);
-const connection  = mongoose.connection;
-connection.once('open', () => {
-    console.log('Mongo DB connected successfully');
-})
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Mongo DB connected successfully");
+});
 
-const userRoutes = require('./routes/users');
-const excerciseRoutes = require('./routes/exercise');
+const userRoutes = require("./routes/users");
+const excerciseRoutes = require("./routes/exercise");
 
-app.use('/users',userRoutes);
-app.use('/excercise',excerciseRoutes);
-app.listen(port , () => {
-    console.log('server is running at port ' + port);
-})
+app.use("/users", userRoutes);
+app.use("/excercise", excerciseRoutes);
+app.listen(port, () => {
+  console.log("server is running at port " + port);
+});
